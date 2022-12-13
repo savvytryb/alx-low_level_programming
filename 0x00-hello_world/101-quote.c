b75b958c25e1023c9eff9e20f0c42dbd01c23a61
kabir <kbdotnigeria@gmail.com>
2022-12-12 19:52:54 -0800

    C world task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>                                                   
-/**                                                                  
-* *  * main - Prints "and that piece of art is useful\n"               
-* *  - Dora Korpar, 2015-10-19",                                       
+/**             
+* *  * main - Prints "and that piece of art is useful\n"
+* *  - Dora Korpar, 2015-10-19", 
 * *   * followed by a new line, to standard error. * * Return: Alwys 1.
-*/                                                          
+*/                                              
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
-return (1);                                          }
+return (1);
+}
